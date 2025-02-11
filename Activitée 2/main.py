459ce5a4cdbc47dec5834a7d0158676737abe4bd
NathanDeMaertelaere <147918113+NathanDeMaertelaere@users.noreply.github.com>
2025-02-11 18:11:16 +0100

    NathanDeMaertelaere

--- Activitée 2/main.py
+++ Activitée 2/main.py
@@ -1,19 +1,17 @@
 # On importe Flask du module Flask
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, request, abort, session
 
 # CREATION DE L'APP
 # On crée une instance de flask qui est donc notre app qu'on stocke dans la variable app
 app = Flask("My first WebApp")
 
-# Adresse IP de l'hôte (modifiez cela selon l'adresse IP de l'hôte)
-HOST_IP = "192.168.1.24"  # Remplacez par votre IP publique ou locale selon le contexte
+HOST_IP = "192.168.1.24" 
 
-# Middleware pour vérifier l'IP du client
 @app.before_request
 def limit_remote_addr():
     client_ip = request.remote_addr
     if client_ip != HOST_IP:
-        abort(403)  # Renvoie une erreur 403 si l'IP ne correspond pas
+        abort(403) 
 
 # Route de la page d'accueil
 @app.route('/')
