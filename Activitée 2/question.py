20bd70c7aff865b05dd3ab804f6601d00d4a6434
NathanDeMaertelaere <147918113+NathanDeMaertelaere@users.noreply.github.com>
2025-02-11 18:37:45 +0100

    NathanDeMaertelaere

--- Activitée 2/question.py
+++ Activitée 2/question.py
@@ -2,5 +2,10 @@ questions = [
     {
         "enonce":"Si tu devais partir en voyage, tu choisirais…"
     },
-    {}
+    {
+        "Link" : 0,
+        "Sheik" : 0, 
+        "Zelda" : 0, 
+        "Tingle" : 0
+    }
 ]
