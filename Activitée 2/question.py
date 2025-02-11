7dc18af3415358fb2c02ec1a6cad1d09a1916484
NathanDeMaertelaere <147918113+NathanDeMaertelaere@users.noreply.github.com>
2025-02-11 18:38:52 +0100

    NathanDeMaertelaere

--- Activitée 2/question.py
+++ Activitée 2/question.py
@@ -3,9 +3,9 @@ questions = [
         "enonce":"Si tu devais partir en voyage, tu choisirais…"
     },
     {
-        "Link" : 0,
-        "Sheik" : 0, 
-        "Zelda" : 0, 
-        "Tingle" : 0
+        "Link" : "une aventure en pleine nature, sac à dos et boussole.",
+        "Sheik" : "une ville mystérieuse pleine de secrets à découvrir.", 
+        "Zelda" : "un endroit paisible où tu pourrais te détendre et profiter du paysage.", 
+        "Tingle" : "un lieu rempli de défis et d’énigmes à résoudre."
     }
 ]
