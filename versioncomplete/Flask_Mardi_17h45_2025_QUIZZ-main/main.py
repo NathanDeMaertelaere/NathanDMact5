75655623b836ef84ec990c260274fc3c71bc50bc
NathanDeMaertelaere <nathan.demaertelaere@gmail.com>
2025-03-11 16:22:36 +0100

    NathanDeMaertelaere

---
+++ versioncomplete/Flask_Mardi_17h45_2025_QUIZZ-main/main.py
@@ -0,0 +1,78 @@
+# On importe Flask du module flask
+from flask import Flask, render_template, session, redirect
+
+# Importation de os
+import os
+
+# On importe la liste de dictionnaires (question + réponses) de notre fichiers questions.py
+from questions import questions
+
+# On importe les descriptions pour le resultats
+from resultats import resultats
+
+# CREATION DE l'APP
+# On crée une instance de Flask qui est donc notre app qu'on stocke dans la variable app
+app = Flask("Mon Super Quizz")
+app.secret_key = os.urandom(24)
+
+# Route de notre page d'accueil qui est donc à la racine de notre app
+@app.route("/")
+def index():
+    session["numero_question"] = 0
+    session["score"] = { "Pikachu" : 0, "Mew" : 0, "Salamèche" : 0, "Carapuce":0}
+    return render_template("index.html")
+
+# Route seconde Page
+@app.route("/question")
+def question():
+    # On accède à la variable globale questions
+    global questions
+    # On récupère le numéro de la question
+    numero = session["numero_question"]
+    # On vérifie que nous en sommes pas à la dernière
+    if numero < len(questions):
+        # On récupère l'énoncé de la question
+        question = questions[numero]["enonce"]
+        # On crée une copie du dictionnaire de notre question
+        questions_copy = questions[numero].copy()
+        # On supprime l'énoncé pour n'avoir que les questions
+        questions_copy.pop("enonce")
+        # On récupère nos réponses sous forme de liste
+        reponses = list(questions_copy.values())
+        # On récupère les clefs = personnages sous forme de liste pour comptage des scores
+        clefs = list(questions_copy.keys())
+        # On stocke l'ordre dans un cookie pour le comptage des scores
+        session["clefs"] = clefs
+        # On affiche la question et les réponses possibles
+        return render_template("question.html", question = question, reponses = reponses)
+    else :
+        # On récupère la variable globale résultats
+        global resultats
+        # On transforme nos score en liste décroissante
+        scores_tries = sorted(session["score"], key = session["score"].get, reverse = True)
+        # On récupère le nom du personnage gagnant =
+        vainqueur = scores_tries[0]
+        # On récupère la description liée au personnage
+        description = resultats[vainqueur]
+        # On affiche notre page de resultat en injectant le nom du vainqueur et la description
+        return render_template("resultats.html", vainqueur = vainqueur, description = description  )
+
+
+
+# Route permettant de compter les réponses de l'utilisateur
+@app.route("/reponse/<numero>")
+def reponse(numero):
+    # On incrémente numero_question pour passer à la question suivante
+    session["numero_question"] += 1
+    # On récupère le nom du vainqueur = le nom du personnage dont la réponse est associée
+    personnage = session["clefs"][int(numero)]
+    # On incrémente le score du personnage dont la réponse est associée
+    session["score"][personnage] += 1
+    # On redirige vers le route question afin d'afficher la question suivante
+    return redirect("/question")
+
+
+# EXECUTION
+# host = "0.0.0.0" -> app accessible par n'importe quelle adresse
+# port = 81 -> port d'écoute du serveur de l'app
+app.run(host = "0.0.0.0", port = 81)
---
+++ versioncomplete/Flask_Mardi_17h45_2025_QUIZZ-main/questions.py
@@ -0,0 +1,30 @@
+questions = [
+    {
+        "enonce": "Si tu devais partir en voyage, tu choisirais…",
+        "Link": "une aventure en pleine nature, sac à dos et boussole.",
+        "Sheik": "une ville mystérieuse pleine de secrets à découvrir.", 
+        "Zelda": "un endroit paisible où tu pourrais te détendre et profiter du paysage.", 
+        "Tingle": "un lieu rempli de défis et d’énigmes à résoudre."
+    },
+    {
+        "enonce": "Tu es coincé(e) dans une pièce fermée, que fais-tu ?",
+        "Link": "Je cherche une sortie, quitte à forcer un peu.",
+        "Sheik": "J’examine tout attentivement pour trouver un mécanisme caché.", 
+        "Zelda": "J’appelle quelqu’un pour demander de l’aide.", 
+        "Tingle": "J’improvise un plan avec ce que j’ai sous la main."
+    },
+    {
+        "enonce": "Quel type de plat te fait le plus envie ?",
+        "Link": "Un bon plat bien consistant qui me redonne de l’énergie.",
+        "Sheik": "Un repas raffiné avec des ingrédients rares.", 
+        "Zelda": "Un plat réconfortant qui me rappelle des souvenirs.", 
+        "Tingle": "Un truc simple mais ingénieux, préparé avec les moyens du bord."
+    },
+    {
+        "enonce": "Dans un groupe d’amis, tu es plutôt…",
+        "Link": "celui/celle qui motive tout le monde à partir à l’aventure.",
+        "Sheik": "celui/celle qui réfléchit et trouve toujours une bonne solution.", 
+        "Zelda": "celui/celle qui veille sur les autres et apaise les tensions.", 
+        "Tingle": "celui/celle qui trouve toujours un moyen de se sortir d’un mauvais pas."
+    }
+]
