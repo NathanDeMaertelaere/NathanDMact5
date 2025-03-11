20f4ae7b930256c766aa0404dc1428ca27999717
NathanDeMaertelaere <nathan.demaertelaere@gmail.com>
2025-03-11 18:13:01 +0100

    'NathanDeMaertelaere'

--- Activitée 2/main.py
+++ Activitée 2/main.py
@@ -8,7 +8,7 @@ import os
 from questions import questions
 
 # On importe les descriptions pour le resultats
-from questions import questions
+from questions import questions as resultats
 
 # CREATION DE l'APP
 # On crée une instance de Flask qui est donc notre app qu'on stocke dans la variable app
