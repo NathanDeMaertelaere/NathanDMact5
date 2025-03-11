975b4a7a0c18967dbc27198990d25f076f925c0e
NathanDeMaertelaere <nathan.demaertelaere@gmail.com>
2025-03-11 18:20:28 +0100

    'NathanDeMaertelaere'

--- Activitée 2/resultats.py
+++ Activitée 2/resultats.py
@@ -4,4 +4,4 @@ resultats = {
     "Sheik": "Tu es mystérieux et stratégique, préférant agir dans l'ombre pour guider et aider les autres.",
     "Zelda": "Tu es sage et bienveillant, doté d'une grande intelligence et d'un profond sens du devoir.",
     "Tingle": "Tu es excentrique et joyeux, apportant une touche de folie et de magie partout où tu vas.",
-};
+}
